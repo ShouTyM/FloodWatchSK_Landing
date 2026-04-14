3779a82f838539f09be271e39dbf64024d068082
ShouTyM <96092910+ShouTyM@users.noreply.github.com>
2026-04-14 14:06:03 +0200

    feat: add Hero component to app root

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/navbar/navbar';
+import { Hero } from './sections/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
