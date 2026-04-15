6c78147c9063e362a0c06a01a42be64d5108a8d0
ShouTyM <96092910+ShouTyM@users.noreply.github.com>
2026-04-15 11:18:41 +0200

    feat: register Problem component in app root

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/navbar/navbar';
 import { Hero } from './sections/hero/hero';
+import { Problem } from './sections/problem/problem';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Hero],
+  imports: [RouterOutlet, Navbar, Hero, Problem],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
